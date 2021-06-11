e0136a92a583f88947a5090ce6d04fb74b16945d
lukman akbar <bellukchips@gmail.com>
2021-06-11 13:47:51 +0700

    popup bootstrap framework

---
+++ script.js
@@ -0,0 +1,19 @@
+$(function() {
+    //show popup modal
+    var myModal = new bootstrap.Modal(document.getElementById('modalPopUp'));
+    myModal.show();
+
+    // jika klik ok maka : 
+    $('#btnOk').click(function() {
+        var audio = new Audio("lagu.mp3");
+        audio.play();
+        //sembunyikan popup
+        myModal.hide();
+    });
+    // jika klik close maka :
+    $('#btnClose').click(function() {
+        alert("Yahh masa gak mau :(")
+            //kembali di redirect ke halaman yang sama
+        window.location.href = "https://tegarwj.github.io/kartuUcapan";
+    });
+});
